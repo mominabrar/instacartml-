83e7f29154d49ef8c7044409c53ada22eed81b06
mominabrar <momin.abrar@gmail.com>
2017-07-16 14:24:58 -0400

    azureprocessing

--- Apriori.R
+++ Apriori.R
@@ -1,8 +1,29 @@
 
 source("dir.R")
+library(doAzureParallel)
+
+# 1. Generate your credential and cluster configuration files.  
+generateClusterConfig("cluster.json")
+generateCredentialsConfig("credentials.json")
+
+# 2. Fill out your credential config and cluster config files.
+# Enter your Azure Batch Account & Azure Storage keys/account-info into your credential config ("credentials.json") and configure your cluster in your cluster config ("cluster.json")
+
+# 3. Set your credentials - you need to give the R session your credentials to interact with Azure
+setCredentials("credentials.json")
+
+# 4. Register the pool. This will create a new pool if your pool hasn't already been provisioned.
+cluster <- makeCluster("cluster.json")
+
+# 5. Register the pool as your parallel backend
+registerDoAzureParallel(cluster)
+
+# 6. Check that your parallel backend has been registered
+getDoParWorkers()
+
+
 
 #creating a sample of orders andd priors
-
 sample_orders <- function(perc, data){
   all_order_id <- unique(orders_train$order_id)
   sample_orders <- sample(all_order_id, 0.3 * length(all_order_id))
